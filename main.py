dc49a88ec634747930a81ebbfa4c2d5340ff66bf
makshat2004 <akshatmamtani7588@gmail.com>
2026-02-05 16:33:17 +0530

    Search agents with tavily

--- main.py
+++ main.py
@@ -1,45 +1,24 @@
-from langchain_core.prompts import PromptTemplate
+from pyexpat import model
 from dotenv import load_dotenv
 load_dotenv()
-from langchain_ollama import ChatOllama
-
-import warnings
-warnings.filterwarnings("ignore", message="Core Pydantic V1 functionality isn't compatible with Python 3.14 or greater")
-
+from langchain.agents import create_agent
+from langchain_core.messages import HumanMessage
 from langchain_openai import ChatOpenAI
+from langchain.tools import tool
+from langchain_tavily import TavilySearch
 
-def main():
-  print("Info LLM Call")
+tavily_search = TavilySearch(max_results=5, topic="general")
 
-  info = """
-  
-  Harbhajan Singh (born 3 July 1980), also known by the nickname Bhajji, is a former Indian cricketer. He later became a politician, serving as a Member of Parliament in Rajya Sabha. He is also a film actor, a television celebrity, and a cricket commentator.
+tools = [tavily_search]
 
-  Harbhajan played for India from 1998 to 2016 as an off spin bowler. In domestic cricket, he played for the Punjab cricket team; and in the Indian Premier League for the Mumbai Indians, Chennai Super Kings, and Kolkata Knight Riders. Considered one of the best Indian spin bowlers of his era, he was on the Indian teams that won the 2007 T20 World Cup and the 2011 Cricket World Cup, and also the team that were joint-winners with Sri Lanka of the 2002 ICC Champions Trophy. He was also a lower-order batter, having two centuries in tests with a top score of 115.
-  
-  """
-
-  summary_info = """
-
-    given the information {info} abput a person, I want you to create:
-    1. A short summary
-    2. Two interesing facts about them
-
-  """
-
-  summary_tempelate = PromptTemplate(
-    input_variables=["info"],template=summary_info
-  )
-
-  # llm = ChatOllama(temperature = 0, model="gemma3:270m")
-
-  llm = ChatOpenAI(model="gpt-4.1-nano",temperature=0)
-
-  chain = summary_tempelate | llm
-
-  response = chain.invoke(input = {"info": info})
-
-  print(response.content)
+llm = ChatOpenAI(model="gpt-4.1-nano")
+agent = create_agent(llm, tools , system_prompt="You are a helpful research assistant. Use web search to find accurate, up-to-date information.")
 
 
-main()
+
+if __name__ == "__main__":
+  response = agent.invoke({
+      "messages": [{"role": "user", "content": "Search for 3 job postings on linkedn for pune in india for a ai engineer role"}]
+  })  
+  print(response)
+
